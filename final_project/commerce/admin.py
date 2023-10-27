f619852bb318cb151b27730d59dc412e43ea2bf5
unknown <khapn21411@st.uel.edu.vn>
2023-10-27 08:54:29 +0700

    Update Category and Item

--- final_project/commerce/admin.py
+++ final_project/commerce/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Category, Item
 
+class ItemAdmin(admin.ModelAdmin):
+    readonly_fields = ['available']
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Item, ItemAdmin)
--- final_project/commerce/models.py
+++ final_project/commerce/models.py
@@ -1,8 +1,33 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class User(AbstractUser):
-    email= models.EmailField(unique=True)
-    
+    email = models.EmailField(unique=True)
+
+class Category(models.Model):
+    category = models.CharField(max_length=64)
+    def __str__(self):
+        return f"{self.category}"
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(max_length=600)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    category = models.ManyToManyField(Category, null=True, related_name="listings")
+    available = models.BooleanField()
+    stock = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(200)])
+    image_URL = models.URLField(_("Image URL(Optinonal)"),blank=True, max_length=200)
+    def save(self, *args, **kwargs):
+        if self.stock == 0:
+            self.available = False
+        elif self.stock != 0:
+            self.available = True
+        elif not self.image_URL:
+            self.image_URL = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+        super(Item, self).save(*args, **kwargs)
+
+
+
