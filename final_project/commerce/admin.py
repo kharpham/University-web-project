d1d05dfc780f1ab10cb4ff6b68a68e239a9d5215
lothikimngan <145126204+lothikimngan@users.noreply.github.com>
2023-10-28 12:28:49 +0700

    ngan

--- final_project/commerce/admin.py
+++ final_project/commerce/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+cd ffrom django.contrib import admin
 from .models import User, Category, Item
 
 class ItemAdmin(admin.ModelAdmin):
--- final_project/commerce/templates/commerce/ngan_HomePage.js
+++ final_project/commerce/templates/commerce/ngan_HomePage.js
@@ -1,33 +1,87 @@
-﻿const formOpenBtn = document.querySelector("#form-open"),
-    home = document.querySelector(".home"),
-    formContainer = document.querySelector(".form_container"),
-    formCloseBtn = document.querySelector(".form_close"),
-    signupBtn = document.querySelector("#signup"),
-    loginBtn = document.querySelector("#login"),
-    pwShowHide = document.querySelectorAll(".pw_hide");
-
-formOpenBtn.addEventListener("click", () => home.classList.add("show"));
-formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
-
-pwShowHide.forEach((icon) => {
-    icon.addEventListener("click", () => {
-        let getPwInput = icon.parentElement.querySelector("input");
-        if (getPwInput.type === "password") {
-            getPwInput.type = "text";
-            icon.classList.replace("uil-eye-slash", "uil-eye");
-        } else {
-            getPwInput.type = "password";
-            icon.classList.replace("uil-eye", "uil-eye-slash");
-        }
-    });
+﻿/* Show form */
+const showBtn = document.querySelector(".button_signup");
+const body = document.querySelector(".hero-section");
+const form = document.querySelector(".container");
+const hideBtn = form.querySelector(".close-btn");
+// Show signup
+showBtn.addEventListener("click", () => {
+    document.body.classList.toggle("show");
 });
 
-signupBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    formContainer.classList.add("active");
+// Hide signup
+hideBtn.addEventListener("click", () => showBtn.click());
+
+
+/* Form */
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
+
+nextBtnFirst.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
 });
-loginBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    formContainer.classList.remove("active");
+nextBtnSec.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-50%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+nextBtnThird.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-75%";
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+});
+submitBtn.addEventListener("click", function () {
+    bullet[current - 1].classList.add("active");
+    progressCheck[current - 1].classList.add("active");
+    progressText[current - 1].classList.add("active");
+    current += 1;
+    setTimeout(function () {
+        alert("Your Form Successfully Signed up");
+        location.reload();
+    }, 800);
 });
 
+prevBtnSec.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "0%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnThird.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-25%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
+prevBtnFourth.addEventListener("click", function (event) {
+    event.preventDefault();
+    slidePage.style.marginLeft = "-50%";
+    bullet[current - 2].classList.remove("active");
+    progressCheck[current - 2].classList.remove("active");
+    progressText[current - 2].classList.remove("active");
+    current -= 1;
+});
